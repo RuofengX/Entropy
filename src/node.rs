1b5e1c557d11a5128aadf146ae2c6d59a9a514c4
ruofeng <i@ruofengx.cn>
2024-04-03 21:14:31 +0800

    Add node test

--- src/node.rs
+++ src/node.rs
@@ -1,76 +1,77 @@
-use serde::{de::Visitor, ser::SerializeSeq, Deserialize, Serialize};
+use rand::{thread_rng, Rng};
+use serde::{Deserialize, Serialize};
 use std::{
     hash::Hash,
     io::{Read, Write},
 };
 
-#[derive(Debug)]
-pub struct NodeData([u8; 1024]);
-impl Serialize for NodeData {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        // Transparent storage, ignore newtype
-        serializer.serialize_bytes(&self.0)
+#[derive(Debug, Clone, Copy, Deserialize, Serialize, PartialEq, Eq)]
+pub struct NodeData(#[serde(with = "serde_bytes")] pub [u8; 1024]);
+impl NodeData {
+    pub fn random() -> Self {
+        let mut rtn = [0u8; 1024];
+        thread_rng().fill(&mut rtn);
+        Self(rtn)
     }
 }
-impl<'de> Deserialize<'de> for NodeData {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        struct NodeDataVisitor;
-        impl<'de> Visitor<'de> for NodeDataVisitor {
-            type Value = NodeData;
-            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                formatter.write_str("")
-            }
 
-            fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
-            where
-                E: serde::de::Error,
-            {
-                let mut rtn = [0u8; 1024];
-                if v.take(1024).read(&mut rtn).is_ok() {
-                    Ok(NodeData(rtn))
-                } else {
-                    Err(E::custom(format!(
-                        "byte length of node data not correct(!=1024)"
-                    )))
-                }
-            }
+#[derive(Debug, Default, Clone, Copy, Serialize, Deserialize, PartialEq, Eq)]
+pub struct NodeID(pub i16, pub i16);
+impl NodeID {
+    pub fn get_nearby_index(&self) -> NearbyIndex {
+        let mut rtn = [self.clone(); 9];
+        let directions: [(i16, i16); 9] = [
+            (-1, 1),
+            (0, 1),
+            (1, 1),
+            (-1, 0),
+            (0, 0),
+            (1, 0),
+            (-1, -1),
+            (0, -1),
+            (1, -1),
+        ];
+
+        for (node, direction) in rtn.iter_mut().zip(directions.iter()) {
+            (*node).0 += direction.0;
+            (*node).1 += direction.1;
         }
-        deserializer.deserialize_bytes(NodeDataVisitor)
+        NearbyIndex(rtn)
     }
 }
 
-#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
-pub struct NodeID((i16, i16));
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Node {
-    id: NodeID,
-    index: NodeIndex,
+    index: NearbyIndex,
     data: NodeData,
 }
 impl Node {
+    pub fn new(id: NodeID, data: NodeData) -> Self {
+        Node {
+            index: id.get_nearby_index(),
+            data,
+        }
+    }
+
     pub fn save_to_writer(self, writer: impl Write) {
         serde_json::to_writer(writer, &self)
-            .expect(format!("error when write node {:?} into writer", self.id).as_str());
+            .expect(format!("error when write node {:?} into writer", self.index.get_id()).as_str());
     }
+
     pub fn construct_from_reader(reader: impl Read) -> Self {
         serde_json::from_reader(reader).expect("error when read from reader")
     }
 }
 
-/// 0,1,2
-/// 3,4,5,
-/// 6,7,8
-#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
-pub struct NodeIndex(pub [NodeID; 9]);
-impl NodeIndex {
-    pub fn get_self_id(&self) -> NodeID {
+/// y
+/// ^ 0,1,2
+/// | 3,4,5,
+/// | 6,7,8
+/// |------> x
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq, Eq)]
+pub struct NearbyIndex(pub [NodeID; 9]);
+impl NearbyIndex {
+    pub fn get_id(&self) -> NodeID {
         self.0[4]
     }
 }
@@ -79,13 +80,48 @@ impl NodeIndex {
 pub struct FlatIndex(pub i32);
 impl From<NodeID> for FlatIndex {
     fn from(value: NodeID) -> Self {
-        FlatIndex((value.0 .0 as i32) << 16 | (value.0 .1) as i32)
+        FlatIndex((value.0 as i32) << 16 | (value.1) as i32)
     }
 }
 impl Into<NodeID> for FlatIndex {
     fn into(self) -> NodeID {
         let high = (self.0 >> 16) as i16;
         let low = self.0 as i16;
-        NodeID((high, low))
+        NodeID(high, low)
+    }
+}
+
+mod test {
+
+    #[test]
+    fn test_node_serde() {
+        use super::{Node, NodeData, NodeID};
+        let node_i = NodeID(1, 2);
+        let node = Node::new(node_i, NodeData::random());
+        let encoded_string = serde_json::to_string(&node).unwrap();
+        let encoded_pickle =
+            serde_pickle::to_vec(&node, serde_pickle::SerOptions::default()).unwrap();
+        let encoded_bin =
+            bincode::serde::encode_to_vec(node.clone(), bincode::config::standard()).unwrap();
+
+        // println!("{}", encoded_string);
+        println!("{:?}", encoded_pickle);
+        println!("json格式长度 -> {}", encoded_string.len());
+        println!("pickle格式长度 -> {}", encoded_pickle.len());
+        println!("bincode格式长度 -> {}", encoded_bin.len());
+
+        ////////////////////
+
+        let node2 = serde_json::from_str::<Node>(&encoded_string).unwrap();
+        assert_eq!(node, node2);
+
+        let node3 =
+            serde_pickle::from_slice::<Node>(&encoded_pickle, serde_pickle::DeOptions::default())
+                .unwrap();
+        assert_eq!(node, node3);
+
+        let (node4, _): (Node, usize) =
+            bincode::serde::decode_from_slice(&encoded_bin, bincode::config::standard()).unwrap();
+        assert_eq!(node, node4);
     }
 }
