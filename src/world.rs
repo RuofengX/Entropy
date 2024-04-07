7c23038fd083d2bce512f6d7ab1016a2d8b9ba61
WeiRuofeng <i@ruofengx.cn>
2024-04-07 23:46:12 +0800

    Add increase fn for world

--- src/world.rs
+++ src/world.rs
@@ -1,3 +1,5 @@
+use std::sync::RwLock;
+
 use ahash::AHashMap;
 use serde::{Deserialize, Serialize};
 
@@ -17,12 +19,32 @@ pub trait SaveStorage {
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, Serialize, Deserialize)]
 pub struct WorldID(pub u64);
+impl WorldID {
+    pub fn get_then_increase(&mut self) -> WorldID {
+        let rtn = self.clone();
+        self.0 += 1;
+        rtn
+    }
+}
 
 pub struct World {
     id: WorldID,
     players: AHashMap<GID, Guest, ahash::RandomState>,
     nodes_active: AHashMap<NodeID, Node, ahash::RandomState>,
     storage_backend: Box<dyn SaveStorage>,
+}
+
+impl World {
+    pub fn new(storage_backend: Box<dyn SaveStorage>) -> World {
+        static WORLD_ID: RwLock<WorldID> = RwLock::new(WorldID(0));
+        let mut wtx = WORLD_ID.write().unwrap();
+        World {
+            id: wtx.get_then_increase(),
+            players: AHashMap::new(),
+            nodes_active: AHashMap::new(),
+            storage_backend,
+        }
+    }
 }
 
 impl World {
